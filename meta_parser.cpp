c331454e16559fe7f01c93f94831056a7d0cf82d
Joshalosh <josh.jessopsmith@gmail.com>
2023-08-20 22:57:00 +1000

    Add custom memory allocation

--- meta_parser.cpp
+++ meta_parser.cpp
@@ -1,32 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint>
+#include <stdint.h>
 
-typedef uint8_t  u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-
-typedef int8_t  s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
-
-typedef float  f32;
-typedef double f64;
-typedef int32_t b32;
-
-enum Token_Type
-{
+enum Token_Type {
     Token_Unknown,
 
     Token_OpenParen,
     Token_CloseParen,
+    Token_Asterisk,
+    Token_Colon,
+    Token_Semicolon,
     Token_OpenBracket,
     Token_CloseBracket,
-    Token_Colon,
-    Token_SemiColon,
-    Token_Asterisk,
+    Token_OpenBrace,
+    Token_CloseBrace,
 
     Token_String,
     Token_Identifier,
@@ -37,20 +24,48 @@ enum Token_Type
 struct Token {
     Token_Type type;
 
-    size_t text_length;
     char *text;
+    size_t text_length; 
 };
 
-char *ReadEntireFileIntoMemoryAndNullTerminate(char *filename) {
-    char *result = 0;
+struct Memory_Arena {
+    char *start;
+    char *current;
+    char *end;
+};
 
-    FILE *file = fopen(filename, "r");
+Memory_Arena InitArena(size_t size) {
+    Memory_Arena result;
+    result.start   = (char *)malloc(size);
+    result.current = result.start;
+    result.end     = result.current + size;
+
+    return result;
+}
+
+void *ArenaAlloc(Memory_Arena *arena, size_t size) {
+    void *result = NULL;
+
+    if (arena->current + size <= arena->end) {
+        result          = arena->current;
+        arena->current += size;
+    } else {
+        printf("There's no room in the arena");
+    }
+
+    return result;
+}
+
+char *ReadEntireFileIntoMemoryAndNullTerminate(Memory_Arena *arena, char *filename) {
+    char *result = NULL;
+    FILE *file   = fopen(filename, "r");
+
     if (file) {
         fseek(file, 0, SEEK_END);
         size_t file_size = ftell(file);
         fseek(file, 0, SEEK_SET);
 
-        result = (char *)malloc(file_size + 1);
+        result = (char *)ArenaAlloc(arena, file_size);
         fread(result, file_size, 1, file);
         result[file_size] = 0;
 
@@ -60,3 +75,6 @@ char *ReadEntireFileIntoMemoryAndNullTerminate(char *filename) {
     return result;
 }
 
+int main()
+{
+};
